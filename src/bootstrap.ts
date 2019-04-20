0fc01a3ec4cbe63bbeb1c1dd5ad23b6b15019613
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-04-20 12:06:40 +0200

    :zap: Only import register

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,7 @@
-import tsConfigPaths from 'tsconfig-paths';
+import { register } from 'tsconfig-paths';
 import tsConfig from '../tsconfig.json';
 
-tsConfigPaths.register({
+register({
   baseUrl: tsConfig.compilerOptions.baseUrl,
   paths: tsConfig.compilerOptions.paths,
 });
