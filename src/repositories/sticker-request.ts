d527b95b175301da1e017bee92eff70acfde409f
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-04-26 08:29:25 +0200

    :card_file_box: Added StickerRequest repository

---
+++ src/repositories/sticker-request.ts
@@ -0,0 +1,54 @@
+import StickerRequest, { Definition } from 'models/sticker-request';
+
+export const add = async (data: {
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  street?: string;
+  bus?: string | null;
+  postalCode?: string;
+  city?: string;
+  note?: string | null;
+  emailConfirmedAt?: Date | null;
+  rejectedAt?: Date | null;
+  sentAt?: Date | null;
+}): Promise<Definition> => {
+  const stickerRequest = await StickerRequest.create(data);
+
+  return stickerRequest.get({ plain: true });
+};
+
+export const getById = async (id: string): Promise<Definition | null> => {
+  const stickerRequest = await StickerRequest.findOne({ where: { id } });
+
+  if (stickerRequest) {
+    return stickerRequest.get({ plain: true });
+  }
+
+  return null;
+};
+
+export const update = async (
+  id: string,
+  data: {
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    street?: string;
+    bus?: string | null;
+    postalCode?: string;
+    city?: string;
+    note?: string | null;
+    emailConfirmedAt?: Date | null;
+    rejectedAt?: Date | null;
+    sentAt?: Date | null;
+  },
+): Promise<void> => {
+  await StickerRequest.update(data, { where: { id } });
+};
+
+export default {
+  add,
+  getById,
+  update,
+};
