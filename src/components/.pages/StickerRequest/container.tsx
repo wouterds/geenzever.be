53a9c54370c7850afe0bdc8ad9b154d730949681
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-04-26 08:57:54 +0200

    :recycle: Confirm email using dedicated endpoint

--- src/components/.pages/StickerRequest/container.tsx
+++ src/components/.pages/StickerRequest/container.tsx
@@ -1,24 +1,60 @@
-import { INTERNAL_SERVER_ERROR, OK } from 'http-status';
+import { INTERNAL_SERVER_ERROR, NO_CONTENT, OK } from 'http-status';
 import { ComponentType } from 'react';
 import api from 'services/api';
 import sentry from 'services/sentry';
 
-interface MappedProps {
-  stickerRequest: {
-    id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    street: string;
-    bus: string;
-    postalCode: string;
-    city: string;
-    note: string;
-    emailConfirmedAt: Date | null;
-    rejectedAt: Date | null;
-    sentAt: Date | null;
-  } | null;
+interface StickerRequest {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  street: string;
+  bus: string;
+  postalCode: string;
+  city: string;
+  note: string;
+  emailConfirmedAt: Date | null;
+  rejectedAt: Date | null;
+  sentAt: Date | null;
 }
+
+interface MappedProps {
+  stickerRequest: StickerRequest | null;
+}
+
+const getStickerRequest = async (
+  id: string,
+): Promise<StickerRequest | null> => {
+  try {
+    const { status, data } = await api.get(`/sticker-requests/${id}`);
+
+    if (status !== OK) {
+      return null;
+    }
+
+    return data;
+  } catch (e) {
+    sentry.captureException(e);
+  }
+
+  return null;
+};
+
+const confirmStickerRequest = async (id: string): Promise<boolean> => {
+  try {
+    const { status } = await api.post(`/sticker-requests/${id}/confirm-email`);
+
+    if (status !== NO_CONTENT) {
+      return false;
+    }
+
+    return true;
+  } catch (e) {
+    sentry.captureException(e);
+  }
+
+  return false;
+};
 
 export default function withContainer<Props>(
   WrappedComponent: ComponentType<Props>,
@@ -31,21 +67,14 @@ export default function withContainer<Props>(
     const { query, isServer, res } = ctx;
     const { id } = query;
 
-    let stickerRequest = null;
-
+    let stickerRequest: StickerRequest | null = null;
     if (id) {
-      try {
-        const { status, data } = await api.get(`/sticker-requests/${id}`);
-
-        if (status === OK) {
-          stickerRequest = data;
-        }
-      } catch (e) {
-        sentry.captureException(e);
-      }
+      stickerRequest = await getStickerRequest(id);
     }
 
-    if (!stickerRequest) {
+    if (stickerRequest) {
+      await confirmStickerRequest(id);
+    } else {
       res.statusCode = INTERNAL_SERVER_ERROR;
     }
 
