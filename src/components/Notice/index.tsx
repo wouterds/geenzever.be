8ebe4783365ad8a40d37fd6860281e771a044fe0
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-04-29 08:25:45 +0200

    :wheelchair: Always show notice

--- src/components/Notice/index.tsx
+++ src/components/Notice/index.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Container } from './styles';
 
 export default () => {
   const { t } = useTranslation();
-  const [isVisible, setIsVisible] = useState(false);
+  // const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    setIsVisible(true);
-  }, [true]);
+  // useEffect(() => {
+  //   setIsVisible(true);
+  // }, [true]);
 
-  if (!isVisible) {
-    return null;
-  }
+  // if (!isVisible) {
+  //   return null;
+  // }
 
   return (
     <Container>
